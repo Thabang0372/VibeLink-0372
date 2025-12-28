79b556568c62b600fce700bb9de34d92a39d0c61
Thabang Agondo <seventhlegendptyltd@gmail.com>
2025-12-28 18:24:09 +0200

    Add files via upload

---
+++ Constants.js
@@ -0,0 +1,36 @@
+const Constants = {
+    APP_NAME: 'VibeLink0372',
+    VERSION: '1.0.0',
+    
+    // User roles
+    ROLES: {
+        USER: 'user',
+        MODERATOR: 'moderator',
+        ADMIN: 'admin'
+    },
+    
+    // Notification types
+    NOTIFICATION_TYPES: {
+        MESSAGE: 'message',
+        LIKE: 'like',
+        COMMENT: 'comment',
+        FOLLOW: 'follow',
+        EVENT: 'event'
+    },
+    
+    // Privacy settings
+    PRIVACY: {
+        PUBLIC: 'public',
+        FRIENDS: 'friends',
+        PRIVATE: 'private'
+    },
+    
+    // Currency
+    CURRENCY: {
+        DEFAULT: 'VIBE',
+        USD: 'USD',
+        EUR: 'EUR'
+    }
+};
+
+export default Constants;
