b1a1680870c040df7954cb0ebcc1602f50d971de
Thabang Agondo <seventhlegendptyltd@gmail.com>
2026-05-02 23:22:54 +0200

    Add files via upload

--- Constants.js
+++ Constants.js
@@ -1,36 +1,8 @@
-const Constants = {
+window.Constants = {
     APP_NAME: 'VibeLink0372',
     VERSION: '1.0.0',
-    
-    // User roles
-    ROLES: {
-        USER: 'user',
-        MODERATOR: 'moderator',
-        ADMIN: 'admin'
-    },
-    
-    // Notification types
-    NOTIFICATION_TYPES: {
-        MESSAGE: 'message',
-        LIKE: 'like',
-        COMMENT: 'comment',
-        FOLLOW: 'follow',
-        EVENT: 'event'
-    },
-    
-    // Privacy settings
-    PRIVACY: {
-        PUBLIC: 'public',
-        FRIENDS: 'friends',
-        PRIVATE: 'private'
-    },
-    
-    // Currency
-    CURRENCY: {
-        DEFAULT: 'VIBE',
-        USD: 'USD',
-        EUR: 'EUR'
-    }
+    ROLES: { USER: 'user', MODERATOR: 'moderator', ADMIN: 'admin' },
+    NOTIFICATION_TYPES: { MESSAGE: 'message', LIKE: 'like', COMMENT: 'comment', FOLLOW: 'follow', EVENT: 'event' },
+    PRIVACY: { PUBLIC: 'public', FRIENDS: 'friends', PRIVATE: 'private' },
+    CURRENCY: { DEFAULT: 'VIBE', USD: 'USD', EUR: 'EUR' }
 };
-
-export default Constants;
