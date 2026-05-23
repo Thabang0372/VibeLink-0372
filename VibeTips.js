ee7f91b787f0aca0067b624371e4c57dc94378eb
Thabang Agondo <seventhlegendptyltd@gmail.com>
2026-05-23 14:19:13 +0200

    Add files via upload

--- VibeTips.js
+++ VibeTips.js
@@ -7,15 +7,12 @@ class VibeTips {
                 creator: { type: 'Pointer', targetClass: '_User', required: true },
                 amount: { type: 'Number', required: true },
                 currency: { type: 'String', default: 'VIBE' },
-                message: { type: 'String' },
-                createdAt: { type: 'Date' }
+                message: { type: 'String' }
             }
         };
     }
-
     static createParseClass() {
         return Parse.Object.extend('VibeTips');
     }
 }
-
-export default VibeTips;
+window.VibeTips = VibeTips;
