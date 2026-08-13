bd890efa9a5fce43b497cfa55b821b0308d65bcb
Thabang Agondo <seventhlegendptyltd@gmail.com>
2026-08-13 13:10:20 +0200

    Add files via upload

--- VibeChallenge.js
+++ VibeChallenge.js
@@ -8,7 +8,7 @@ class VibeChallenge {
                 mediaTemplate: { type: 'File' },
                 startDate: { type: 'Date' },
                 endDate: { type: 'Date' },
-                participants: { type: 'Relation', targetClass: '_User' },
+                participants: { type: 'Array', default: [] },      // changed from Relation to Array
                 reward: { type: 'Number', default: 0 },
                 trendScore: { type: 'Number', default: 0 }
             }
